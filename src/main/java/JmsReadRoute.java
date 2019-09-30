5f8eb12bff1dadcf88aefb0b87c23da25dee2ab4
komarov-alt <koskon@inbox.ru>
2019-09-30 16:13:56 +0500

    Added a body check for emptiness

--- src/main/java/JmsReadRoute.java
+++ src/main/java/JmsReadRoute.java
@@ -1,13 +1,16 @@
+import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.model.DataFormatDefinition;
 
-import static org.apache.activemq.camel.component.ActiveMQComponent.activeMQComponent;
 public class JmsReadRoute extends RouteBuilder {
     public void configure() throws Exception {
         from("activemq:queue:testQueue")
-                .to("log:?level=INFO&showBody=true")
-                .unmarshal().base64()
-                .to("activemq:queue:readQueue")
-                .to("log:?level=INFO&showBody=true");
+                .choice()
+                    .when(body().isNull())
+                        .log(LoggingLevel.INFO,"Is NULL Message")
+                    .when(body().in(""))
+                        .log(LoggingLevel.INFO,"Is Empty Message")
+                    .otherwise()
+                        .unmarshal().base64()
+                        .to("activemq:queue:readQueue");
     }
 }
