42a4a39a6385393d0893b2c4b6d473c6f2b2a769
koskom-alt <55850699+koskom-alt@users.noreply.github.com>
2019-10-02 12:37:46 +0500

    rename artId

--- src/main/java/com/koskom/cameltask/MessageDecoder.java
+++ src/main/java/com/koskom/cameltask/MessageDecoder.java
@@ -7,18 +7,18 @@ import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.camel.CamelContext;
 import org.apache.camel.component.jms.JmsComponent;
 import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.camel.spi.ShutdownStrategy;
 
 public class MessageDecoder {
-    private String brokerUrl;
+    private String url;
 
-    MessageDecoder(String brokerUrl){
-        this.brokerUrl = brokerUrl;
+    MessageDecoder(String url){
+        this.url = url;
     }
-
     public void run() throws Exception {
         JmsReadRoute routeBuilder = new JmsReadRoute();
         CamelContext ctx = new DefaultCamelContext();
-        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(brokerUrl);
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(url);
 
         ctx.addComponent("activemq", JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
 
@@ -33,14 +33,8 @@ public class MessageDecoder {
         }
     }
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
         MessageDecoder messageDecoder = new MessageDecoder (ActiveMQConnection.DEFAULT_BROKER_URL);
-        try {
-            messageDecoder.run ();
-        } catch (Exception e) {
-            e.printStackTrace ( );
-        }
-
+        messageDecoder.run ();
     }
 }
